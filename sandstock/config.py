4db541a8f83e44c7c8844d56567eba2817f5df6d
VictorMeyer77 <=>
2025-02-15 22:30:53 +0100

    build: add env for az publish

--- sandstock/config.py
+++ sandstock/config.py
@@ -2,7 +2,12 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{os.getenv('ENV')}-{os.getenv('PROJECT')}-sql.postgres.database.azure.com:5432/{os.getenv('ENV')}_erp?user=psql_admin&password={os.getenv('SQL_DB_ADMIN_PASSWORD')}&sslmode=require"
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+psycopg2://"
+        f"{os.getenv('ENV')}-{os.getenv('PROJECT')}-sql.postgres.database.azure.com:5432/"
+        f"{os.getenv('ENV')}_erp?user=psql_admin&password={os.getenv('SQL_DB_ADMIN_PASSWORD')}"
+        f"&sslmode=require"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(32)
     LOGOUT_URL = f"https://login.microsoftonline.com/{os.getenv('TENANT_ID')}/oauth2/v2.0/logout"
